634c39ab1aa56326f5a67dc9fd8d55e4c1c30a2e
Adarsh Kumar <adarsh010c@gmail.com>
2023-09-04 23:36:37 +0530

    dayone part2 intial test sucessful.

--- adventofcode2021/dayOne_test.go
+++ adventofcode2021/dayOne_test.go
@@ -5,6 +5,19 @@ import (
 )
 
 func TestDayOne(t *testing.T) {
+	comapreSlice := func(got, want []int, t *testing.T) {
+		t.Helper()
+		gotLen, wantLen := len(got), len(want)
+		if gotLen == wantLen {
+			for i, _ := range got {
+				if got[i] != want[i] {
+					t.Fatalf("slices are unequal, got : %v ,wanted: %v", got, want)
+				}
+			}
+		} else {
+			t.Errorf("lengths are unequal, got length: %d, want length: %d", gotLen, wantLen)
+		}
+	}
 	t.Run("count number of increased measurements", func(t *testing.T) {
 		input := ReadInput("input.txt")
 		got := DayOne(input)
@@ -13,6 +26,13 @@ func TestDayOne(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d, want %d", got, want)
 		}
+	})
+
+	t.Run("three-measurement slide window", func(t *testing.T) {
+		input := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+		got := GetSlideList(input)
+		want := []int{607, 618, 618, 617, 647, 716, 769, 792}
+		comapreSlice(got, want, t)
 	})
 
 }
