f94b0ee655e385d792f13464e7dd5d1868b4ccf9
Adarsh Kumar <adarsh010c@gmail.com>
2024-01-03 16:20:11 +0530

    added util file

---
+++ 2023/dayOne/util/util.go
@@ -0,0 +1,22 @@
+package util
+
+import(
+	"log",
+	"os",
+	"strings"
+)
+
+
+//Takes in a txt file ,returns content as slice of string 
+func ReadInput(fileName string) []string {
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	contentStr := string(content)
+	//remove \r\n
+	contentStrList := strings.Split(contentStr, "\r\n")
+
+	
+	return contentStrList
+} 
